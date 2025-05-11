d0a8d0cf8349f5a788cf70d7828425f381ba521e
Jaishree <jaishreeofficial29@gmail.com>
2025-05-11 19:34:27 +0530

    urls

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -2,7 +2,7 @@ class MyUrls {
   static const String resume =
       'https://drive.google.com/file/d/1CMbz0D6jubqOg4R6qS8SchjYxWQ3Or7G/view?usp=sharing';
   static const String twitter = 'https://x.com/Jaishree_29';
-  static const String linkedin = ''
+  static const String linkedin = '';
       // 'https://www.linkedin.com/in/jaishree-tiwari-54212925b/';
   static const String github = 'https://github.com/jaishree29';
   static const String email = 'itsjaishree.tiwari@gmail.com';
