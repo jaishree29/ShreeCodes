91e9dcd299b0b8674f97f7408a978a24a3781edf
Jaishree <jaishreeofficial29@gmail.com>
2025-11-29 17:46:07 +0530

    Reduced image sizes

--- lib/utils/image_strings.dart
+++ lib/utils/image_strings.dart
@@ -1,21 +1,21 @@
 class MyImages {
   //development skills icons
-  static const String flutterIcon = 'assets/logos/flutter.png';
-  static const String profileImage = 'assets/images/profile-image.png';
-  static const String dartIcon = 'assets/logos/dart.png';
+  static const String flutterIcon = 'assets/logos/flutter.webp';
+  static const String profileImage = 'assets/images/profile-image.webp';
+  static const String dartIcon = 'assets/logos/dart.webp';
   static const String firebaseIcon = 'assets/logos/firebase.png';
   static const String gitIcon = 'assets/logos/git.png';
-  static const String androidIcon = 'assets/logos/android.png';
+  static const String androidIcon = 'assets/logos/android.webp';
   static const String goIcon = 'assets/logos/go.png';
-  static const String cppIcon = 'assets/logos/c++.png';
+  static const String cppIcon = 'assets/logos/c++.webp';
   static const String javaIcon = 'assets/logos/java.png';
 
   // Design skills icons
-  static const String figmaIcon = 'assets/logos/figma.png';
-  static const String userResearchIcon = 'assets/logos/research.png';
-  static const String prototypingIcon = 'assets/logos/prototyping.png';
-  static const String wireframingIcon = 'assets/logos/wireframe.png';
-  static const String uiuxIcon = 'assets/logos/ui-ux.png';
+  static const String figmaIcon = 'assets/logos/figma.webp';
+  static const String userResearchIcon = 'assets/logos/research.webp';
+  static const String prototypingIcon = 'assets/logos/prototyping.webp';
+  static const String wireframingIcon = 'assets/logos/wireframe.webp';
+  static const String uiuxIcon = 'assets/logos/ui-ux.webp';
   // static const String usabilityTestingIcon =
   //     'https://upload.wikimedia.org/wikipedia/commons/3/3f/Usability_testing_icon.png';
   // static const String responsiveDesignIcon =
@@ -24,18 +24,18 @@ class MyImages {
   //     'https://upload.wikimedia.org/wikipedia/commons/3/3f/Animation_icon.png';
 
   // Tools icons
-  static const String vsCodeIcon = 'assets/logos/vscode.png';
-  static const String androidStudioIcon = 'assets/logos/android_studio.png';
+  static const String vsCodeIcon = 'assets/logos/vscode.webp';
+  static const String androidStudioIcon = 'assets/logos/android_studio.webp';
   static const String postmanIcon = 'assets/logos/postman.png';
-  static const String githubIcon = 'assets/logos/github.png';
+  static const String githubIcon = 'assets/logos/github.webp';
   static const String miroIcon = 'assets/logos/miro.png';
-  static const String slackIcon = 'assets/logos/slack.png';
+  static const String slackIcon = 'assets/logos/slack.webp';
   static const String trelloIcon = 'assets/logos/Trello-Emblem.png';
-  static const String notionIcon = 'assets/logos/notion.png';
+  static const String notionIcon = 'assets/logos/notion.webp';
 
   // Projects images
-  static const String thatGirlImage = 'assets/images/ThatGirl.png';
-  static const String resumeBuilderImage = 'assets/images/ResumeBuilder.png';
-  static const String localeezyImage = 'assets/images/Localeezy.jpg';
-  static const String canteenoImage = 'assets/images/Canteeno.jpg';
+  static const String thatGirlImage = 'assets/images/ThatGirl.webp';
+  static const String resumeBuilderImage = 'assets/images/ResumeBuilder.webp';
+  static const String localeezyImage = 'assets/images/Localeezy.webp';
+  static const String canteenoImage = 'assets/images/Canteeno.webp';
 }
